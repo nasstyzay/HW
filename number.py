d756903e5c4e0b52f0d93fc6344659e5c2cc3dea
nasstyzay <nasstyzay@mail.ru>
2024-04-09 12:18:43 +0300

    add

--- number.py
+++ number.py
@@ -4,13 +4,14 @@ import re
 
 
 def normalize_phone_number(phone):
-    pattern = r"(\+7|8)?\s\((\d)\)\s*(\d)[\s-]*(\d)[\s-]*(\d+)"
+    pattern = r"(\+7|8)?\s*\(?(\d{3})\)?[\s-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(?:\s*(доб.)\s*(\d+))?"
     replacement = r"+7(\2)\3-\4-\5"
     corrected_phone = re.sub(pattern, replacement, phone)
-    if "доб." in phone:
-        corrected_phone += f" доб.{re.search(pattern, phone).group('ext')}"
-    return corrected_phone
 
+    match = re.search(pattern, phone)
+    if match and match.group(6):
+        corrected_phone += f" {match.group(6)}{match.group(7)}"
+    return corrected_phone
 
 def process_contacts_list(contacts_list):
     for contact in contacts_list:
@@ -18,7 +19,6 @@ def process_contacts_list(contacts_list):
         while len(full_name) < 3:
             full_name.append("")
         contact[:3] = full_name[:3]
-
 
         contact[5] = normalize_phone_number(contact[5])
 
@@ -35,7 +35,6 @@ def process_contacts_list(contacts_list):
     return list(unique_contacts.values())
 
 
-
 contacts_list = []
 with open("phonebook_raw.csv", encoding="utf-8") as f:
     rows = csv.reader(f, delimiter=",")
@@ -45,6 +44,6 @@ with open("phonebook_raw.csv", encoding="utf-8") as f:
 processed_contacts_list = process_contacts_list(contacts_list)
 
 
-with open("phonebook_raw.csv", "w", encoding="utf-8", newline='') as f:
+with open("phonebook_processed.csv", "w", encoding="utf-8", newline='') as f:
     datawriter = csv.writer(f, delimiter=',')
     datawriter.writerows(processed_contacts_list)
